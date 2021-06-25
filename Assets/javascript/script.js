aef0aa24b4a7a0e3e5654455c7ec3bb01f155970
Kaleb Muse <kcmuse@Kalebs-MacBook-Air.local>
2021-06-24 19:13:11 -0700

    added todo

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,11 +1,15 @@
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
+// TODO: Timer to start upper right hand corner of the page counting down
 // WHEN I answer a question
 // THEN I am presented with another question
+// TODO: when a question is answered another question needs to pop up
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
+// TODO: 
 // WHEN all questions are answered or the timer reaches 0
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and my score
+// TODO: score tracker, where players input initials and the score is tracked
